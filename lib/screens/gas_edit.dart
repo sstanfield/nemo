b52ddefe17ebd2f529bb6456e11ef96206e2373e
Steven Stanfield <stanfield@scarecrowtech.com>
2017-11-04 00:20:41 -0400

    Move gas settings to a validated form.

--- lib/screens/gas_edit.dart
+++ lib/screens/gas_edit.dart
@@ -17,37 +17,68 @@ class _GasEditState extends State<GasEdit> {
   final AppBar _appBar;
   //final BottomNavigationBar _botNavBar;
   final Gas _gas;
-  final TextEditingController _o2;
-  final TextEditingController _he;
   bool _decoGas = false;
   final Segment _segment;
+  int _pO2;
+  int _pHe;
   var _save;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text(value)
+    ));
+  }
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      //_autovalidate = true;  // Start validating on every change.
+      showInSnackBar('Please fix the errors in red before submitting.');
+    } else {
+      form.save();
+      Gas newGas = !_decoGas?new Gas.bottom(_pO2 / 100.0, _pHe / 100.0, 1.4):new Gas.deco(_pO2 / 100.0, _pHe / 100.0);
+      _save(_segment, _gas, newGas);
+    }
+  }
+
+  String _validateGas(String gas) {
+    int igas = -1;
+    if (gas.length == 0) igas = 0;
+    else try { igas = int.parse(gas); } catch (ignored) {}
+    if (igas < 0 || igas > 100) return "Enter gas percent 0-100";
+    return null;
+  }
 
   _GasEditState(this._appBar, this._gas, this._save, this._segment):
-        _o2 = new TextEditingController(text: "${(_gas.fO2*100).round()}"),
-        _he = new TextEditingController(text: "${(_gas.fHe*100).round()}");
+        _pO2 = (_gas.fO2*100).round(),
+        _pHe = (_gas.fHe*100).round();
 
   @override
   Widget build(BuildContext context) {
     Column c3 = new Column(children: [
-      new TextField(controller: _o2, decoration: new InputDecoration(labelText:  "O2 %:"), keyboardType: TextInputType.number),
-      new TextField(controller: _he, decoration: new InputDecoration(labelText:  "He %:"), keyboardType: TextInputType.number),
+      new TextFormField(initialValue: "$_pO2",
+          onSaved: (String val) => _pO2 = val.length==0?0:int.parse(val),
+          validator: _validateGas,
+          decoration: new InputDecoration(labelText:  "O2 %:"), keyboardType: TextInputType.number),
+      new TextFormField(initialValue: "$_pHe",
+          onSaved: (String val) => _pHe = val.length==0?0:int.parse(val),
+          validator: _validateGas,
+          decoration: new InputDecoration(labelText:  "He %:"), keyboardType: TextInputType.number),
       new Row(
         children: [new Checkbox(value: _decoGas, onChanged: (bool v) => setState(() {_decoGas = v;})), const Text('Deco Gas') ]
       ),
       new FlatButton(
         child: const Text('Save'),
-        onPressed: () {
-          Gas newGas = !_decoGas?new Gas.bottom(int.parse(_o2.text) / 100.0, int.parse(_he.text) / 100.0, 1.4):new Gas.deco(int.parse(_o2.text) / 100.0, int.parse(_he.text) / 100.0);
-          _save(_segment, _gas, newGas);
-        },
+        onPressed: _handleSubmitted,
       ),
     ]);
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: _appBar,
-      body: c3,
+      body: new Form(key: _formKey, child: c3),
       //bottomNavigationBar: _botNavBar,
-      //floatingActionButton: _floatingActionButton,
     );
   }
 }
