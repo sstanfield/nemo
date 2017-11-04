17b5d4aa99fe124739b758e1f926a7976bda5831
Steven Stanfield <stanfield@scarecrowtech.com>
2017-11-04 00:40:02 -0400

    Move segment settings to a validated form.

--- lib/screens/dive_segment.dart
+++ lib/screens/dive_segment.dart
@@ -18,8 +18,72 @@ class _DiveSegmentState extends State<DiveSegment> {
   final AppBar _appBar;
   final int ceiling;
   final int index;
-  final TextEditingController _depthcontroller;
-  final TextEditingController _timecontroller;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  int _depth;
+  int _time;
+
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text(value)
+    ));
+  }
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      //_autovalidate = true;  // Start validating on every change.
+      showInSnackBar('Please fix the errors in red before submitting.');
+    } else {
+      form.save();
+      List<Segment> segments = new List<Segment>();
+      Segment lastSegment;
+      int i = 0;
+      for (final s in _dive.segments.where((Segment s) => !s.isCalculated)) {
+        if (s.type == SegmentType.LEVEL) {
+          int tmptime = s.time+(lastSegment!=null&&lastSegment.type!=SegmentType.LEVEL?lastSegment.time:0);
+          if (index == i) {
+            segments.add(s.isSurfaceInterval?(new Segment.surfaceInterval(_time)..addAllGasses(s.gasses)):
+            new Segment(s.type, _depth, 0.0, _time, s.gas, false, 0));
+          } else {
+            segments.add(s.isSurfaceInterval?(new Segment.surfaceInterval(s.time)..addAllGasses(s.gasses)):
+            new Segment(s.type, _dive.mbarToDepthM(s.depth),
+                0.0, tmptime, s.gas, false, s.ceiling));
+          }
+        }
+        lastSegment = s;
+        i++;
+      }
+      _dive.clearSegments();
+      lastSegment = null;
+      for (Segment s in segments) {
+        if (s.depth > 0) _dive.move(lastSegment == null ? 0 : lastSegment.depth, s.depth, s.time);
+        else _dive.addSurfaceInterval(s.time).addAllGasses(s.gasses);
+        lastSegment = s;
+      }
+      if (index == -1) {
+        if (_depth > 0) _dive.move(lastSegment==null?0:lastSegment.depth, _depth, _time);
+        else _dive.addSurfaceInterval(_time).addAllGasses(_dive.dives.first.gasses);
+      }
+      Navigator.of(context).pop();
+    }
+  }
+
+  String _validateDepth(String depth) {
+    int idepth = -1;
+    if (depth.length == 0) idepth = 0;
+    else try { idepth = int.parse(depth); } catch (ignored) {}
+    if (idepth < 0 || idepth > 1000) return "Enter depth 0-1000";
+    return null;
+  }
+
+  String _validateTime(String time) {
+    int itime = -1;
+    if (time.length == 0) itime = 0;
+    else try { itime = int.parse(time); } catch (ignored) {}
+    if (itime < 0 || itime > 1000) return "Enter time 0-1000";
+    return null;
+  }
 
   static int _getDepth(Dive dive, int index) {
     int i = 0;
@@ -41,9 +105,7 @@ class _DiveSegmentState extends State<DiveSegment> {
     return 10;
   }
 
-  _DiveSegmentState(this._appBar, this._dive, this.index, this.ceiling):
-        _depthcontroller = new TextEditingController(text: "${_getDepth(_dive, index)}"),
-        _timecontroller = new TextEditingController(text: "${_getTime(_dive, index)}");
+  _DiveSegmentState(this._appBar, this._dive, this.index, this.ceiling);
 
   @override
   Widget build(BuildContext context) {
@@ -51,49 +113,23 @@ class _DiveSegmentState extends State<DiveSegment> {
         padding: const EdgeInsets.all(20.0),
         children: <Widget>[
           new Text("Ceiling: $ceiling"),
-          new TextField(controller: _depthcontroller, decoration: new InputDecoration(labelText:  "Depth:"), keyboardType: TextInputType.number),
-          new TextField(controller: _timecontroller, decoration: new InputDecoration(labelText:  "Time:"), keyboardType: TextInputType.number),
+          new TextFormField(initialValue: "${_getDepth(_dive, index)}",
+              onSaved: (String val) => _depth = val.length==0?0:int.parse(val),
+              validator: _validateDepth,
+              decoration: new InputDecoration(labelText:  "Depth:"), keyboardType: TextInputType.number),
+          new TextFormField(initialValue: "${_getTime(_dive, index)}",
+              onSaved: (String val) => _time = val.length==0?0:int.parse(val),
+              validator: _validateTime,
+              decoration: new InputDecoration(labelText:  "Time:"), keyboardType: TextInputType.number),
           new FlatButton(
             child: const Text('Save'),
-            onPressed: () {
-              int depth = int.parse(_depthcontroller.text);
-              int time = int.parse(_timecontroller.text);
-              List<Segment> segments = new List<Segment>();
-              Segment lastSegment;
-              int i = 0;
-              for (final s in _dive.segments.where((Segment s) => !s.isCalculated)) {
-                if (s.type == SegmentType.LEVEL) {
-                  int tmptime = s.time+(lastSegment!=null&&lastSegment.type!=SegmentType.LEVEL?lastSegment.time:0);
-                  if (index == i) {
-                    segments.add(s.isSurfaceInterval?(new Segment.surfaceInterval(time)..addAllGasses(s.gasses)):
-                      new Segment(s.type, depth, 0.0, time, s.gas, false, 0));
-                  } else {
-                    segments.add(s.isSurfaceInterval?(new Segment.surfaceInterval(s.time)..addAllGasses(s.gasses)):
-                      new Segment(s.type, _dive.mbarToDepthM(s.depth),
-                        0.0, tmptime, s.gas, false, s.ceiling));
-                  }
-                }
-                lastSegment = s;
-                i++;
-              }
-              _dive.clearSegments();
-              lastSegment = null;
-              for (Segment s in segments) {
-                if (s.depth > 0) _dive.move(lastSegment == null ? 0 : lastSegment.depth, s.depth, s.time);
-                else _dive.addSurfaceInterval(s.time).addAllGasses(s.gasses);
-                lastSegment = s;
-              }
-              if (index == -1) {
-                if (depth > 0) _dive.move(lastSegment==null?0:lastSegment.depth, depth, time);
-                else _dive.addSurfaceInterval(time).addAllGasses(_dive.dives.first.gasses);
-              }
-              Navigator.of(context).pop();
-            },
+            onPressed: _handleSubmitted,
           ),
         ]);
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: _appBar,
-      body: c3,
+      body: new Form(key: _formKey, child: c3),
     );
   }
 }
