bbcf82a244964c556bd0ac8e8498db5dc5f5cd96
Steven Stanfield <stanfield@scarecrowtech.com>
2017-11-17 18:54:33 -0500

    Added headers to dive plan.

--- lib/screens/dive_plan.dart
+++ lib/screens/dive_plan.dart
@@ -3,6 +3,9 @@ import '../deco/plan.dart';
 
 class DivePlan extends StatelessWidget {
   final Plan _plan;
+  static final headerStyle = const TextStyle(fontWeight: FontWeight.bold, color: Colors.black,);
+  static final diveNameStyle = const TextStyle(fontWeight: FontWeight.bold, color: Colors.black,);
+  static final highPPO2Style = new TextStyle(fontWeight: FontWeight.bold, color: Colors.red);
 
   DivePlan(this._plan);
 
@@ -22,7 +25,7 @@ class DivePlan extends StatelessWidget {
       diveNum++;
       runtime = 0;
       rows.add(new Row(children: [
-        new Column(children: col1),
+        new Expanded(child: new Column(children: col1)),
         new Expanded(child: new Column(children: col2)),
         new Expanded(child: new Column(children: col3)),
         new Expanded(child: new Column(children: col4)),
@@ -35,27 +38,38 @@ class DivePlan extends StatelessWidget {
       col4 = new List<Widget>();
       col5 = new List<Widget>();
       col6 = new List<Widget>();
-      rows.add(new Text("Dive #$diveNum ${dive.surfaceInterval>0?" Surface Interval ${dive.surfaceInterval} minutes":""}"));
+      col1.add(new Text("Type", style: headerStyle));
+      col2.add(new Text("Depth", style: headerStyle));
+      col3.add(new Text("Stop", style: headerStyle));
+      col4.add(new Text("Runtime", style: headerStyle));
+      col5.add(new Text("Gas", style: headerStyle));
+      col6.add(new Text("PPO2", style: headerStyle));
+      if (diveNum > 1) rows.add(new Divider(height: 24.0,));
+      rows.add(new Text(
+          "Dive #$diveNum ${dive.surfaceInterval>0?" Surface Interval ${dive.surfaceInterval} minutes":""}",
+          style: diveNameStyle
+      ));
       for (final e in dive.segments) {
         if (e.type == SegmentType.DOWN) col1.add(new Text("DESC"));
         if (e.type == SegmentType.UP) col1.add(new Text("ASC"));
-        if (e.type == SegmentType.LEVEL) col1.add(new Text("---"));
-        col2.add(new Text("${dive.mbarToDepth(e.depth)}"));
+        if (e.type == SegmentType.LEVEL) {
+          if (e.isCalculated) col1.add(new Text("DECO"));
+          else col1.add(new Text("BTM"));
+        }
+        col2.add(new Text("${dive.mbarToDepth(e.depth)}${dive.metric?"M":"ft"}"));
         col3.add(new Text("${e.time}"));
         runtime += e.time;
         col4.add(new Text("$runtime"));
         col5.add(new Text("${e.gas}"));
         double ppo2 = e.gas.fO2 * ((e.depth / 1000));
         if (ppo2 > e.gas.ppo2)
-          col6.add(new Text("${ppo2.toStringAsFixed(2)}",
-              style: new TextStyle(
-                  fontWeight: FontWeight.bold, color: Colors.red)));
+          col6.add(new Text("${ppo2.toStringAsFixed(2)}", style: highPPO2Style));
         else
           col6.add(new Text("${ppo2.toStringAsFixed(2)}"));
       }
     }
     rows.add(new Row(children: [
-      new Column(children: col1),
+      new Expanded(child: new Column(children: col1)),
       new Expanded(child: new Column(children: col2)),
       new Expanded(child: new Column(children: col3)),
       new Expanded(child: new Column(children: col4)),
